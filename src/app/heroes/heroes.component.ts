82bb96417a13b791c5057aaa2bd601cf778906af
Anish Mathew <anishkmathew@gmail.com>
2018-07-05 05:30:51 +0530

    list heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,8 +10,10 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
-    name: 'Anish Mathew'
+    name: 'Windstorm'
   };
+
+  heroes = HEROES;
 
   constructor() { }
 
